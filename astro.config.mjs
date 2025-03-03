9e5eeaf79a34837a46fa8222523b2c62f9239b13
Marek Puurunen <marek.puurunen@gmail.com>
2025-03-03 10:59:51 +0200

    Github actions update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: "static",
+  site: process.env.ASTRO_SITE || "http://localhost:3000",
 });
